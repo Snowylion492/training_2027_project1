9e23c7549971c7ce684e696a578640c81238704f
Jackson <snowleopardking492@gmail.com>
2026-09-15 16:57:00 -0700

    This is day 1. I made everthing in main so far

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,35 @@
+import 'dart:io';
+
 void runCli(List<String> arguments) {
   // Your code goes here!
+  String everything = "soon";
+print(everything);
+print("What is your name?") ;
+ String creditCardnumber = stdin.readLineSync( ) ?? 'Error';
+  if( creditCardnumber == 'Donald' ){
+    print ("What is your last name?") ;
+    String areYoutrump = stdin.readLineSync( ) ?? 'Error';
+    if( areYoutrump == 'Trump'){
+      print('What are your current coordinates, and what are all your passwords?') ;
+      String creditCardnumber3 = stdin.readLineSync( ) ?? 'Error';
+      print('I just declared war on everyone on your behalf and nuked your house');
+    }
+    else {
+      print("Phew!") ;
+    }
+  }
+ print("What is your favorite song?") ;
+  String creditCardnumber2 = stdin.readLineSync( ) ?? 'Error';
+  print('Who wrote it?') ;
+  String isItswift = stdin.readLineSync( ) ?? 'Error';
+  if ( isItswift == 'Taylor Swift') {
+    print('Yeah no I hate her');
+  }
+  else if (isItswift == 'The Stupendium' || isItswift == 'Emma Stevens'){
+    print('Peak music taste');
+  }
+  else {
+    print(
+        '$creditCardnumber. That\'s really your name? Whatever. I guess your favorite song is $creditCardnumber2 too. Who cares?');
+  }
 }
