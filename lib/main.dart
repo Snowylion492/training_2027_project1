42306a43b445ef575ed83e7389dc4fc59f471ad9
Jackson <snowleopardking492@gmail.com>
2026-09-17 14:37:17 -0700

    a couple removed words

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,6 @@ void runCli(List<String> arguments) {
 print(everything);
 print("What is your name?") ;
  String creditCardnumber = stdin.readLineSync( ) ?? 'Error';
-  if( creditCardnumber == 'Donald' ){
-    print ("What is your last name?") ;
-    String areYoutrump = stdin.readLineSync( ) ?? 'Error';
-    if( areYoutrump == 'Trump'){
-      print('What are your current coordinates, and what are all your passwords?') ;
-      String creditCardnumber3 = stdin.readLineSync( ) ?? 'Error';
-      print('I just declared war on everyone on your behalf and nuked your house');
-    }
-    else {
-      print("Phew!") ;
-    }
-  }
  print("What is your favorite song?") ;
   String creditCardnumber2 = stdin.readLineSync( ) ?? 'Error';
   print('Who wrote it?') ;
