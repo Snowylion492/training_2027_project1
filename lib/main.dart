ba5e0f4ba3a64c1b3b3aabaa500ae13d8ddac6c1
Jackson <snowleopardking492@gmail.com>
2026-09-17 15:46:09 -0700

    It's all my day 5 work. Randoms and lists and loops oh my.

--- lib/main.dart
+++ lib/main.dart
@@ -20,10 +20,6 @@ print("What is your name?") ;
     print(
         '$creditCardnumber. That\'s really your name? Whatever. I guess your favorite song is $creditCardnumber2 too. Who cares?');
   }
-  print('I can name some people for you!');
-  List<String> people = [
-    "Jackson, my creator",
-  ];
   var i = 0 ;
   while(i != 100)  {
     i = Random().nextInt(200) ;
@@ -39,6 +35,56 @@ print("What is your name?") ;
     else{
       print('1, 2, 3, 3, huff, 4, 5, 6, 6, 6, huff, 7, 8, 9, 10! Huff, that\'s hard!') ;
     }
+  }
+  print('I can name some people for you!');
+  List<String> people = [
+    "Jackson, my creator",
+    "Donald Trump",
+    "Emma Stevens",
+    "George Washington",
+    "Uhh... Rick!",
+  ];
+  Map<String, String> peopleandhow = {
+    "Jackson, my creator": "He made me",
+    "Donald Trump":"Everyone knows him now!",
+    "Emma Stevens" : "My creator likes her music",
+    "George Washington": "He made the US",
+    "Uhh... Rick!": "https://youtu.be/yvg5ZVwR-N4?si=8IXORpDwbKWTjfCR",
+  };
+  for(var j in people){
+    print(people) ;
+  }
+  print('Oh wait, I know someone else!') ;
+  people.add(creditCardnumber);
+  print(people);
+  print('That\'s ${people.length} people!') ;
+  print('Do you want to know how I know them?') ;
+  String yes = stdin.readLineSync() ?? "Error" ;
+  if( yes == 'No'){
+    print('Well too bad!');
+    print(people[0]);
+    print(peopleandhow["Jackson, my creator"]);
+    print(people[1]);
+    print(peopleandhow["Donald Trump"]);
+    print(people[3]);
+    print(peopleandhow["Emma Stevens"]);
+    print(people[4]);
+    print(peopleandhow["George Washington"]);
+    print(people[5]);
+    print(peopleandhow["Uhh... Rick!"]);
+  }
+  else{
+    print('Ok, here!');
+    print(people[0]);
+    print(peopleandhow["Jackson, my creator"]);
+    print(people[1]);
+    print(peopleandhow["Donald Trump"]);
+    print(people[3]);
+    print(peopleandhow["Emma Stevens"]);
+    print(people[4]);
+    print(peopleandhow["George Washington"]);
+    print(people[5]);
+    print(peopleandhow["Uhh... Rick!"]);
   }
   print('Ok I\'m tired after all that. Goodbye!') ;
 }
