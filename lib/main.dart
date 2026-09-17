b5aa4429cafe36baf7057c9fdd281da048684408
Jackson <snowleopardking492@gmail.com>
2026-09-17 16:56:12 -0700

    The beginning and notes for my project

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 import 'dart:math' ;
 void runCli(List<String> arguments) {
   // Your code goes here!
-  // Categorize team members by youngster or unc
-print('This is the name-based Youngster Or Unc Data storer');
-print('What\' their name?');
-String namethefirst = stdin.readLineSync() ?? "Error" ;
+  // Come up with new ideas
+  Map<String, String> InsertVariable = {
+
+  }
+
 }
