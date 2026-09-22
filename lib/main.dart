28b25a1adb86e0ad12d791cb902410e0fa8a93a9
Jackson <snowleopardking492@gmail.com>
2026-09-22 16:22:04 -0700

    My scouting project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,50 @@
 import 'dart:io';
 import 'dart:math' ;
+Map<String,int> teamsAndWins(String team, int wins) {
+  Map<String, int> teamsAndWinsMap = {
+    team: wins
+  };
+  return teamsAndWinsMap ;
+}
+// function, { user input, user input, add to teamsAndWinsMap, print what I just did}
+Map<String, int> addingTeams(String addedTeam, int newWins) {
+  Map<String, int> teamsAndWinsMap = {
+  addedTeam: newWins
+  };
+  return teamsAndWinsMap ;
+}
+void areYouDone(Map<String, int> teamsAndWinsMap) {
+  print('Are you done? Say Yes exactly if yes. If you want to see all data, say See list') ;
+  String done = stdin.readLineSync() ?? 'No';
+  if(done == 'Yes') {
+    print('Ok, goodbye!') ;
+    exit(0) ;
+  }
+  else if(done == 'See list') {
+print('$teamsAndWinsMap') ;
+  }
+  else{
+    print('What is the team?') ;
+    String team = stdin.readLineSync() ?? 'Error' ;
+    print('How many wins') ;
+    String wins = stdin.readLineSync() ?? 'Error' ;
+    int intWins = int.tryParse(wins) ?? 35505;
+    teamsAndWinsMap[team] = intWins ;
+    print('$team, ${teamsAndWinsMap[team]}');
+  }
+}
 void runCli(List<String> arguments) {
   // Your code goes here!
-  // Come up with new ideas
-  Map<String, String> InsertVariable = {
-
+  // team, matches won
+  print('What is the team?');
+  String team = stdin.readLineSync() ?? 'Error';
+  print('How many wins');
+  String wins = stdin.readLineSync() ?? 'Error';
+  int intWins = int.tryParse(wins) ?? 35505;
+  Map<String, int> teamsAndWinsMap = teamsAndWins(team, intWins);
+  print('$team, ${teamsAndWinsMap[team]}');
+  int y = 0;
+  for(y = 0; y == 0; ) {
+    areYouDone(teamsAndWinsMap);
   }
-
 }
