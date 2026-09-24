7fe3a2560f28afb7780a474e1ba7f234e3a18c80
Jackson <snowleopardking492@gmail.com>
2026-09-24 15:34:18 -0700

    My scouting project halfway cause I have to storming use nested maps.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,11 @@
 import 'dart:io';
-import 'dart:math' ;
-// Nested Map example)
-// Map<String, dynamic> userProfile = {
-// 'name': 'Alex',
-// 'contact': {
-// 'email': 'alex@example.com',
-// 'phone': '123-456-7890'
-// }
-// };
-// come up with more questions
-Map<String,int> teamsAndWins(String team, int wins) {
-  Map<String, int> teamsAndWinsMap = {
-    team: wins
-  };
-  return teamsAndWinsMap ;
+List<Data> teamsAndWins(String team, int intWins, int intNumber, String teamRanking) {
+  List<Data> teamsAndWinsList = [
+    Data(team, intNumber, teamRanking, intWins)
+  ];
+  return teamsAndWinsList ;
 }
-// function, { user input, user input, add to teamsAndWinsMap, print what I just did}
-Map<String, int> addingTeams(String addedTeam, int newWins) {
-  Map<String, int> teamsAndWinsMap = {
-  addedTeam: newWins
-  };
-  return teamsAndWinsMap ;
-}
-void areYouDone(Map<String, int> teamsAndWinsMap) {
+void areYouDone(List<Data> teamsAndWinsList) {
   print('Are you done? Say \'yes\' exactly if yes. If you want to see all data, say \'see list\'') ;
   String done = stdin.readLineSync() ?? 'No';
   if(done.toLowerCase() == 'yes') {
@@ -30,31 +13,45 @@ void areYouDone(Map<String, int> teamsAndWinsMap) {
     exit(0) ;
   }
   else if(done.toLowerCase() == 'see list') {
-print('$teamsAndWinsMap') ;
+    print('$teamsAndWinsList') ;
   }
   else{
-    print('What is the team?') ;
-    String team = stdin.readLineSync() ?? 'Error' ;
-    print('How many wins') ;
-    String wins = stdin.readLineSync() ?? 'Error' ;
+    print('What is the team\'s chosen name?');
+    String team = stdin.readLineSync() ?? 'Error';
+    print('What is the team\'s number?');
+    String teamNumber = stdin.readLineSync() ?? 'Error';
+    print('What is the team\'s current ranking?');
+    int intNumber = int.tryParse(teamNumber) ?? 35505;
+    String teamRanking = stdin.readLineSync() ?? 'Error';
+    print('How many wins do they have?');
+    String wins = stdin.readLineSync() ?? 'Error';
     int intWins = int.tryParse(wins) ?? 35505;
-    teamsAndWinsMap[team] = intWins ;
-    print('$team, ${teamsAndWinsMap[team]}');
+    teamsAndWinsList.add(Data(team, intNumber, teamRanking, intWins));
+    print(teamsAndWinsList);
   }
+}
+class Data {
+  String typeValueA ;
+  int typeValueB;
+  String typeValueC;
+  int typeValueD;
+  Data(this.typeValueA, this.typeValueB, this.typeValueC, this.typeValueD);
 }
 void runCli(List<String> arguments) {
-  // Your code goes here!
-  // team, matches won
-  print('What is the team?');
+  print('What is the team\'s chosen name?');
   String team = stdin.readLineSync() ?? 'Error';
-  print('How many wins');
+  print('What is the team\'s number?');
+  String teamNumber = stdin.readLineSync() ?? 'Error';
+  print('What is the team\'s current ranking?');
+  int intNumber = int.tryParse(teamNumber) ?? 35505;
+  String teamRanking = stdin.readLineSync() ?? 'Error';
+  print('How many wins do they have?');
   String wins = stdin.readLineSync() ?? 'Error';
   int intWins = int.tryParse(wins) ?? 35505;
-  Map<String, int> teamsAndWinsMap = teamsAndWins(team, intWins);
-  print('$team, ${teamsAndWinsMap[team]}');
+List<Data> teamsAndWinsList= teamsAndWins(team, intWins, intNumber, teamRanking);
+  print(teamsAndWinsList);
   int y = 0;
   for(y = 0; y == 0; ) {
-    areYouDone(teamsAndWinsMap);
+    areYouDone(teamsAndWinsList);
   }
 }
-
